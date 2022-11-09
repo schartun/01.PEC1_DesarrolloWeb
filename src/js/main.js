2bf6c400debd8724ce61d3d0773387465cc866b1
schartun <schartun@gmail.com>
2022-11-09 11:18:52 +0100

    Set swiperpackage

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,20 @@
 // core version + navigation, pagination modules:
 import Swiper, { Navigation, Pagination } from 'swiper';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+
+// configure Swiper to use modeles
+Swiper.use([Navigation, Pagination]);
 
 // init Swiper:
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'vertical',
+  direction: 'horizontal',
   loop: true,
+  autoplay: true,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
   // Navigation arrows
